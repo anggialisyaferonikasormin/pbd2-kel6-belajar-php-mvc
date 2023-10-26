a57affa82aa0e1ea1d203b853feb7b0468a19da5
anggialisyaferonikasormin <anggisormin60@gmail.com>
2023-10-26 16:42:23 +0700

    PerbaikanUserRepositoryTest

--- php-login-management/app/Repository/UserRepository.php
+++ php-login-management/app/Repository/UserRepository.php
@@ -2,44 +2,47 @@
 
 namespace ProgrammerZamanNow\Belajar\PHP\MVC\Repository;
 
-use ProgramerZamanNow\Belajar\PHP\MVC\Domain\User;
-
+use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
 
 class UserRepository
 {
     private \PDO $connection;
+
     public function __construct(\PDO $connection)
     {
-        $this->connection = $connection;
+        $this->connection= $connection;
     }
-    public function save(User $user): User {
-        $statement = $this->connection->prepare("INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
-        $statement ->execute([
+    public function save(User $user): User{
+        $statement=$this->connection->prepare("INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
+        $statement->execute([
             $user->id, $user->name, $user->password
         ]);
         return $user;
-
     }
-    public function findById(string  $id): ?User {
+
+    public function findById(string $id): ?user
+    {
         $statement = $this->connection->prepare("SELECT id, name, password FROM users WHERE id = ?");
         $statement->execute([$id]);
 
-        try{
-        if ($row = $statement->fetch()){
-            $user = new User();
-            $user->id = $row['id'];
-            $user->name = $row['name'];
-            $user->password = $row['password'];
-            return $user;
+        try {
+            if($row = $statement->fetch()){
+                $user = new User();
+                $user->id = $row['id'];
+                $user->name = $row['name'];
+                $user->password = $row['password'];
 
-        }else {
-            return null;
-        }
-    } finally {
-            $statement->closeCursor();
+                return $user;
+            } else{
+                return null;
             }
+
+        } finally {
+
         }
-public function deleteAll():void{
+    }
+
+    public function deleteAll():void {
         $this->connection->exec("DELETE from users");
-}
+    }
 }
--- php-login-management/test/Repository/UserRepositoryTest.php
+++ php-login-management/test/Repository/UserRepositoryTest.php
@@ -8,31 +8,35 @@ use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
 
 class UserRepositoryTest extends TestCase
 {
-private UserRepository $userRepository;
-protected function setUp(): void
-{
-    $this->userRepository = new UserRepository(Database::getConnection());
-    $this->userRepository->deleteAll();
-}
-public function testSaveSuccess()
-{
-    $user = new User();
-    $user->id = "eko";
-    $user->name = "Eko";
-    $user->password = "rahasia";
 
-    $this->userRepository->save($user);
+    private UserRepository $userRepository;
 
-    $result = $this->userRepository->findById($user->id);
+    protected function setUp(): void
+    {
+        $this->userRepository = new UserRepository(Database::getConnection());
+        $this->userRepository->deleteAll();
+    }
 
-    self::assertEquals($user->id, $result->id);
-    self::assertEquals($user->name, $result->name);
-    self::assertEquals($user->password, $result->password);
-}
-public function testFindByIdNotFound()
-{
-    $user = $this->userRepository->findById("notfound");
-    self::assertNull($user);
-}
+    public function testSaveSuccess()
+    {
+        $user = new User();
+        $user->id = "2";
+        $user->name = "Eko";
+        $user->password = "rahasia";
+
+        $this->userRepository->save($user);
+
+        $result = $this->userRepository->findByid($user->id);
+
+        self::assertEquals($user->id, $result->id);
+        self::assertEquals($user->name, $result->name);
+        self::assertEquals($user->password, $result->password);
+    }
+
+    public function testFindByIdNotFound()
+    {
+        $user = $this->userRepository->findByid("notfound");
+        self::assertNull($user);
+    }
 
 }
